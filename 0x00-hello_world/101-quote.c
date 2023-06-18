428dc49326f7d888a43c9366a1beef550615b208
Mwangidan <mwangiduncan870@gmail.com>
2023-06-18 08:23:04 -0700

    Ooooh betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
 # include <unistd.h>
 /**
- * main - A program that prints aline to the standard error
- * Return 1 (Pass)
+ * main - A C program that prints a line to the standard error
+ * Return: 1 (Pass)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }

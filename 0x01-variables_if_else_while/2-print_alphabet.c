df11db3850152847e213f7d1a8175aa02aa08f3a
Mwangidan <mwangiduncan870@gmail.com>
2023-06-28 12:01:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * mani - A program that prints the alphabet in lowercase
+ * Return: Always 0 (Pass)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+
+		}
+	putchar('\n');
+	return (0);
+}
